ef452a18a63bc98d2dd82de3b8c176767933f3af
test00123 <109013696+test00123@users.noreply.github.com>
2022-07-10 13:44:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -494,16 +494,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "group_rules":
-        buttons = [[
-            InlineKeyboardButton('close , callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.GROUP_R_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     elif query.data == "autofilter":
         buttons = [[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='help')
